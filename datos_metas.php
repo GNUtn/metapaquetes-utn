222916c91ef5a07db6b76557b8637942ffd1b4de
Matias Garcia Isaia <mgarciaisaia+github@gmail.com>
2013-06-13 02:56:34 -0300

    Genera metapaquetes a partir de datos de un array en PHP
    Apesta, apesta y apesta. Pero genera metapaquetes que funcionan.

---
+++ datos_metas.php
@@ -0,0 +1,399 @@
+<?php
+
+	$i=0;
+	
+	{
+		$paquete[++$i]="sistemas-frba";
+		$nombre[$i]="Ingeniería en Sistemas de la FRBA";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="sistemas-frba-1, sistemas-frba-2, sistemas-frba-3, sistemas-frba-4, sistemas-frba-5";
+	}
+	
+	{
+		$paquete[++$i]="sistemas-frba-1";
+		$nombre[$i]="1er año de Ingeniería en Sistemas de la FRBA";
+		$que_instala[$i]="el ".$nombre[$i];
+		$dependencias[$i]="analisis-matematico-1-frba, algebra-y-geometria-analitica-frba, matematica-discreta-frba, sistemas-y-organizaciones-frba, algoritmos-y-estructuras-de-datos-frba, arquitectura-de-computadores-frba, quimica-sistemas-frba, ingenieria-y-sociedad-sistemas-frba";
+	}
+	
+	{
+		$paquete[++$i]="sistemas-frba-2";
+		$nombre[$i]="2do año de Ingeniería en Sistemas de la FRBA";
+		$que_instala[$i]="el ".$nombre[$i];
+		$dependencias[$i]="fisica-1-frba, analisis-matematico-2-frba, sistemas-de-representacion-frba, analisis-de-sistemas-frba, sintaxis-y-semantica-de-los-lenguajes-frba, paradigmas-de-programacion-frba, ingles-1-frba, probabilidad-y-estadistica-frba";
+	}
+	
+	{
+		$paquete[++$i]="sistemas-frba-3";
+		$nombre[$i]="3er año de Ingeniería en Sistemas de la FRBA";
+		$que_instala[$i]="el ".$nombre[$i];
+		$dependencias[$i]="sistemas-operativos-frba, disenio-de-sistemas-frba, fisica-2-frba, matematica-superior-frba, gestion-de-datos-frba, economia-frba, ingles-2-frba, legislacion-frba";
+	}
+	
+	{
+		$paquete[++$i]="sistemas-frba-4";
+		$nombre[$i]="4to año de Ingeniería en Sistemas de la FRBA";
+		$que_instala[$i]="el ".$nombre[$i];
+		$dependencias[$i]="redes-de-informacion-frba, administracion-de-recursos-frba, investigacion-operativa-frba, simulacion-frba, ingenieria-en-software-frba, teoria-de-control-frba, comunicaciones-frba";
+	}
+	
+	{
+		$paquete[++$i]="sistemas-frba-5";
+		$nombre[$i]="5to año de Ingeniería en Sistemas de la FRBA";
+		$que_instala[$i]="el ".$nombre[$i];
+		$dependencias[$i]="proyecto-final-frba, inteligencia-artificial-frba, administracion-general-frba, sistemas-de-gestion-frba";
+	}
+	
+	{
+		$paquete[++$i]="analisis-matematico-1-frba";
+		$nombre[$i]="Análisis Matemático 1 de la FRBA";
+		$que_instala[$i]="la materia ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="algebra-y-geometria-analitica-frba";
+		$nombre[$i]="Álgebra y Geometría Analítica de la FRBA";
+		$que_instala[$i]="la materia ".$nombre[$i];
+		$dependencias[$i]="wxmaxima, xmaxima";
+	}
+	
+	{
+		$paquete[++$i]="matematica-discreta-frba";
+		$nombre[$i]="Matemática Discreta de la FRBA";
+		$que_instala[$i]="la materia ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="sistemas-y-organizaciones-frba";
+		$nombre[$i]="Sistemas y Organizaciones de la FRBA";
+		$que_instala[$i]="la materia ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="algoritmos-y-estructuras-de-datos-frba";
+		$nombre[$i]="Algoritmos y Estructuras de Datos de la FRBA";
+		$que_instala[$i]="la materia ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="arquitectura-de-computadores-frba";
+		$nombre[$i]="Arquitectura de Computadores de la FRBA";
+		$que_instala[$i]="la materia ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="quimica-sistemas-frba";
+		$nombre[$i]="Química (de Sistemas) de la FRBA";
+		$que_instala[$i]="la materia ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="ingenieria-y-sociedad-sistemas-frba";
+		$nombre[$i]="Ingeniería y Sociedad (de Sistemas) de la FRBA";
+		$que_instala[$i]="la materia ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	/**
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	{
+		$paquete[++$i]="";
+		$nombre[$i]="";
+		$que_instala[$i]="la carrera de ".$nombre[$i];
+		$dependencias[$i]="";
+	}
+	
+	
+**/
+?>
